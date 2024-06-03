7682cf62b364ccfe269e5ad7e2f18af77e9a06cd
Andrada0606 <152401493+Andrada0606@users.noreply.github.com>
2024-06-03 21:50:44 +0300

    Update admin.php

--- admin.php
+++ admin.php
@@ -5,6 +5,21 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/foundation.css">
     <link rel="stylesheet" href="css/app.css">
+    <style>
+      
+        body, html {
+            height: 100%;
+            margin: 0;
+            padding: 0;
+        }
+        .container {
+            min-height: 100%;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            flex-direction: column;
+        }
+    </style>
 </head>
 <body>
     <div class="top-bar">
@@ -15,18 +30,20 @@
         </div>
         <div class="top-bar-left">
             <ul class="menu">
-                <li><a href="acasa.html">Acasă</a></li>
+                <li><a href="index.html">Acasă</a></li>
                 <li><a href="Contact.html">Contact</a></li>
                 <li><a href="Despre.html">Despre</a></li>
             </ul>
         </div>
+    </div>
+
+    <div class="top-bar" style="background-color: white;">
         <div class="top-bar-right" style="float: right;">
             <a class="button success" href="index.html">Logout</a>
         </div>
+        <div style="clear: both;"></div>
     </div>
-   <br> 
-   <br>
-</body>
+
     <div class="top-bar">
         <div class="top-bar-right" style="display: block; margin-left: auto; margin-right: auto;">
           <ul class="menu">
@@ -37,57 +54,64 @@
             </ul>
         </div>
     </div>
-    
-    <br><br>
-    <div class="top-bar-center">
-        <ul class="menu">
-            <li><input type="search" placeholder="Search"></li>
-            <li><a href="cauta_imprumut.html"><button type="button" class="button">Search</button></a></li>
-        </ul>
-    </div>
-    <br><br>
+</body>
+    <div class="content">
+        <h1>Împrumuturi</h1>
+        <button class="add-button" onclick="document.getElementById('addForm').style.display='block'">Adaugă Împrumut</button>
+        <form id="addForm" action="adauga_imprumut.php" method="post" style="display: none; margin-top: 20px;">
+    <label for="cod_tranzactie">Cod Tranzacție:</label>
+    <input type="text" id="cod_tranzactie" name="cod_tranzactie" required>
+    <label for="titlu">Carte Imprumutata</label>
+    <input type="text" id="carte_imprumutata" name="carte_imprumutata" required>
+    <label for="nume">Nume Utilizator:</label>
+    <input type="text" id="utilizator" name="utilizator" required>
+    <label for="data_imprumut">Data Împrumutului:</label>
+    <input type="date" id="data_imprumut" name="data_imprumut" required>
+    <label for="data_scadenta">Data Scadenței:</label>
+    <input type="date" id="data_scadenta" name="data_scadenta" required>
+    <label for="data_scadenta">Stare::</label>
+    <input type="text" id="stare" name="stare" required>
+    <input type="submit" value="Adaugă">
+</form>
 
-    <table>
-        <thead>
-            <tr>
-                <th>Cod tranzacție</th>
-                <th>Cărți împrumutate</th>
-                <th>ID Utilizator</th> 
-                <th>Nume Utilizator</th>
-                <th>Data împrumutului</th>
-                <th>Data scadentă</th>
-                <th>Stare</th>
-                <th>Acțiuni</th>
-            </tr>
-        </thead>
-        <tbody>
-            <tr>
-                <td>123456</td>
-                <td>Harry Potter și Piatra Filozofală, Knife</td>
-                <td>1</td> 
-                <td>Pop Alin</td>
-                <td>2024-04-21</td>
-                <td>2024-05-21</td>
-                <td>În curs de procesare</td>
-                <td>
-                 
-                    <button>Anulare</button>
-                </td>
-            </tr>
-            <tr>
-                <td>127832</td>
-                <td>Dans Macabre</td>
-                <td>2</td> 
-                <td>Dan Ana</td> 
-                <td>2024-04-30</td>
-                <td>2024-05-30</td>
-                <td>În curs de procesare</td>
-                <td>
-                  
-                    <button>Anulare</button>
-                </td>
-            </tr>
-        </tbody>
-    </table>
+        <table>
+            <thead>
+                <tr>
+                    <th>ID Împrumut</th>
+                    <th>Titlu Carte</th>
+                    <th>Nume Utilizator</th>
+                    <th>Data Împrumutului</th>
+                    <th>Status</th>
+                    <th>Acțiuni</th>
+                </tr>
+            </thead>
+            <tbody>             
+                <tr>
+                    <td>789012</td>
+                    <td>Rural Hours</td>
+                    <td>Dan Ana</td>
+                    <td>2024-04-20</td>
+                    <td>În așteptare</td>
+                    <td>
+    <form action="procesare_imprumut.php" method="post" style="display: inline;">
+        <input type="hidden" name="imprumut_id" value="789012">
+        <button class="process-button" type="submit" name="process-button">Procesare</button>
+    </form>
+    <form action="anulare_imprumut.php" method="post" style="display: inline;">
+        <input type="hidden" name="imprumut_id" value="789012">
+        <button class="cancel-button" type="submit" name="cancel-button">Anulare</button>
+    </form>
+    <form action="sterge_imprumut.php" method="post" style="display: inline;">
+        <input type="hidden" name="imprumut_id" value="789012">
+        <button class="delete-button" type="submit" name="delete-button">Șterge</button>
+    </form>
+    <a href="vizualizare_imprumut.php?imprumut_id=789012" class="button view-button" style="display: inline-block;">Vizualizare</a>
+</td>
+
+                </tr>
+               
+            </tbody>
+        </table>
+    </div>
 </body>
 </html>
