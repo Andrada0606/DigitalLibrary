fb2fb2c78ffea6190754ccad5b460a89901fae50
Andrada0606 <152401493+Andrada0606@users.noreply.github.com>
2024-06-03 21:13:15 +0300

    Update adauga_utilizatori.php

--- adauga_utilizatori.php
+++ adauga_utilizatori.php
@@ -4,7 +4,6 @@
     <meta charset="utf-8">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Adaugarea in BD</title>
     <link rel="stylesheet" href="https://dhbhdrzi4tiry.cloudfront.net/cdn/sites/foundation.min.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/motion-ui@1.2.3/dist/motion-ui.min.css" />
     <link rel="stylesheet" href="css/app.css">
