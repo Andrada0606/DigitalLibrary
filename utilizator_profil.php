53d1778e06ffb64a86b414a1d74f766609c29f9f
Andrada0606 <152401493+Andrada0606@users.noreply.github.com>
2024-06-03 22:20:49 +0300

    Create utilizator_profil.php

---
+++ utilizator_profil.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Profil Utilizator</title>
+    <link rel="stylesheet" href="css/app.css">
+    <link rel="stylesheet" href="css/foundation.css">
+
+
+    <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Acasa</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/foundation/6.7.3/css/foundation.min.css">
+    <link rel="stylesheet" href="css/app.css">
+</head>
+<div class="top-bar">
+    <div class="top-bar-left">
+        <ul class="menu">
+            <li><h1 style="margin: 0;"><a href="#">DigitalLibrary</a></h1></li>
+        </ul>
+    </div>
+    <div class="top-bar-left">
+        <ul class="menu">
+            <li><a href="utilizator.html">Acasă</a></li>
+            <li><a href="Contact.html">Contact</a></li>
+            <li><a href="Despre.html">Despre</a></li>
+        </ul>
+    </div>
+</div>
+   <div class="top-bar-right" style="float: right;">
+    <a class="button success" href="utilizator.php">Nume</a>
+    <a class="button success" href="index.html">Logout</a>
+
+</div>
+
+<div style="clear: both;"></div>
+</div>
+
+
+<div class="top-bar">
+
+</head>
+<body>
+    <div class="container">
+        <!-- <h1>Profil Utilizator</h1> -->
+        
+        <div class="profile-picture">
+            <img src="user.jpg" alt="">
+        </div>
+        
+        <div class="profile-details">
+            <h2>Nume: Man Andrada</h2>
+            <p>Email: andradaman299@gmail.com</p>
+            <p>Username: andrada21</p>
+        </div>
+
+        <div class="edit-profile-link">
+            <a href="editeaza_profil.html">Editează Profilul <i class="fas fa-user-edit"></i></a>
+        </div>
+
+        <div class="logout-link">
+            <a href="index.html">Logout <i class="fas fa-sign-out-alt"></i></a>
+        </div>
+    </div>
+</body>
+</html>
