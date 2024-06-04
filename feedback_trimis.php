08408bb2f8a868c30f711d1cc5f32cb95ec0107f
Andrada0606 <152401493+Andrada0606@users.noreply.github.com>
2024-06-04 13:08:04 +0300

    Create feedback_trimis.php

---
+++ feedback_trimis.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Acasa</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/foundation/6.7.3/css/foundation.min.css">
+    <link rel="stylesheet" href="css/app.css">
+</head>
+<div class="top-bar">
+    <div class="top-bar-left">
+        <ul class="menu">
+            <li><h1 style="margin: 0;"><a href="#">DigitalLibrary</a></h1></li>
+        </ul>
+    </div>
+    <div class="top-bar-left">
+        <ul class="menu">
+            <li><a href="utilizator.php">Acasă</a></li>
+            <li><a href="Contact.html">Contact</a></li>
+            <li><a href="Despre.html">Despre</a></li>
+        </ul>
+    </div>
+</div>
+   <div class="top-bar-right" style="float: right;">
+    <a class="button success" href="utilizator_profil.php">Nume</a>
+    <a class="button success" href="index.html">Logout</a>
+
+</div>
+
+<div style="clear: both;"></div>
+</div>
+
+
+<div class="top-bar">
+
+<div class="top-bar-right"  style="display: block; margin-left: auto; margin-right: auto;">
+    <ul class="menu">
+        <li><a href="utilizator.php">Acasă</a></li>
+        <li><a href="comenzi_utilizator.html">Comenziile mele</a></li>
+        <li><a href="utilizator_feedback.html"> Feedback</a></li>
+
+        <li><a href="lista_de_dorinte.html">Lista de dorinte</a></li>
+        <li><a href="utilizator_rezervare.html">Rezerva</a></li>
+
+    </ul>
+</div>
+</div>
+<body>
+    <header>
+        <h1>Feedback Trimis</h1>
+    </header>
+    <main>
+        <section class="feedback-confirmation">
+            <h2>Mulțumim pentru feedback!</h2>
+            <p>Feedback-ul tău a fost trimis cu succes. Îți mulțumim pentru timpul acordat și pentru contribuția ta la îmbunătățirea serviciilor noastre.</p>
+            <p>Dacă ai întrebări suplimentare sau alte comentarii, te rugăm să ne contactezi la <a href="mailto:digitallibrary@gmail.com">digitallibrary@gmail.com</a>.</p>
+            <a href="utilizator.php" class="button">Înapoi</a>
+        </section>
+    </main>
+</body>
+</html>
